40832f7179423385f472fe5589e89909ea7d5443
Hydr8O <bananov888@gmail.com>
2021-12-13 20:58:36 +0800

    add reference script, fix install

---
+++ inference.py
@@ -0,0 +1,2 @@
+import os
+os.system('tools/test.py configs/nucleus_model.py --format-only --options "jsonfile_prefix=./answer"')
--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch -y
 
 pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.6.0/index.html
-
-cd mmdetection
 pip install -r requirements/build.txt
 pip install -v -e .
