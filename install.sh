2d9fab24a23c34228abb987f3e749cf1de322697
Hydr8O <bananov888@gmail.com>
2021-12-13 20:35:55 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+conda create -n hw3_env python=3.7 -y
+conda activate hw3_env
+
+conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch -y
+
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.6.0/index.html
+
+cd mmdetection
+pip install -r requirements/build.txt
+pip install -v -e .
